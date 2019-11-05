944452348b06f8e08c05879a81bdf04a4817ee66
Paulo <paulo.correa@indt.org.br>
2019-11-05 09:17:08 -0400

    test_busca_email_fail_findByEmail

--- src/test/java/com/wallet/repository/UserRepositoryTest.java
+++ src/test/java/com/wallet/repository/UserRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.wallet.repository;
 
 import com.wallet.entity.User;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
@@ -15,8 +21,25 @@ import static org.junit.Assert.assertNotNull;
 @ActiveProfiles("test")
 public class UserRepositoryTest {
 
+    private static final String EMAIL = "email@test.com";
+
     @Autowired
     UserRepository repository;
+
+    @Before
+    public void setUp(){
+        User u = new User();
+        u.setName("Set up User");;
+        u.setPassword("Senha123");
+        u.setEmail(EMAIL);
+
+        repository.save(u);
+    }
+
+    @After
+    public void tearDown(){
+        repository.deleteAll();
+    }
 
     @Test
     public void testSave(){
@@ -25,8 +48,15 @@ public class UserRepositoryTest {
         u.setPassword("123456");
         u.setEmail("paulordie@gmail.com");
 
-         User response = repository.save(u);
+        User response = repository.save(u);
 
-         assertNotNull(response);
+        assertNotNull(response);
+    }
+
+    public void testFindByEMail(){
+        Optional<User> response = repository.fynByEmail(EMAIL);
+
+        assertTrue(response.isPresent());
+        assertEquals(response.get().getEmail(), EMAIL);
     }
 }
