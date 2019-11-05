25e4bb4240bda44fd2b4358054b7571b4b929e4b
Paulo <paulo.correa@indt.org.br>
2019-11-05 10:08:20 -0400

    create_UserService_incompleto

---
+++ src/main/java/com/wallet/service/UserServiceTest.java
@@ -0,0 +1,43 @@
+package com.wallet.service;
+
+import com.wallet.entity.User;
+import com.wallet.repository.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class UserServiceTest {
+
+    @MockBean
+    UserRepository repository;
+
+    @Autowired
+    UserService service;
+
+    @Before
+    public void setUp(){
+        //quando o metodo do repositorio foi acionado por findByEmailEquals que vai retornar um novo User mockado vai passar qualquer string para la e mockar o resultado
+        BDDMockito.given(repository.findByEmailEquals(Mockito.anyString())).willReturn(Optional.of(new User()));
+    }
+
+    @Test
+    public void testFindbyEmail(){
+        Optional<User> user = service.findByEmail("email@teste.com");
+
+        assertTrue(user.isPresent());
+    }
+}
